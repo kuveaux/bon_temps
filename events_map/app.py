8763cdf1762ee45390fba1bdbe2161bb58a01108
akufoy <akufoy@gmail.com>
2018-03-29 16:54:33 -0500

    updating app.js and app.py

--- events_map/app.py
+++ events_map/app.py
@@ -1,21 +1,32 @@
 from flask import Flask
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
 
+# flask setup
 app = Flask(__name__)
+# database setup
+Base = automap_base()
+engine = create_engine()
+Base.prepare(engine, reflect=True)
+Events = Base.classes.events
+session = Session(engine)
 
-
-# define static routes
+# template rendering
 @app.route("/")
-def index():
-    return 
+def home():
+    return render_template("index.html")
 
-
-@app.route("/about")
-def about():
-    return 
-
-
-@app.route("/contact")
-def contact():
+# route - leaflet
+@app.route("/api/leaflet")
+def events_geo_data():
+    # PostGRES r
     return 
 
 if __name__ == "__main__":
---
+++ events_map/static/js/app.js
@@ -0,0 +1,46 @@
+var container =  document.getElementById("map_area")
+
+// leaflet javascript -
+
+var queryEvents = "/api/leaflet"
+
+// create markers
+d3.json(queryEvents, createMarkers);
+
+function createMarkers(response) {
+    
+    var markers = []
+    // loop thru endpoint data to create markers
+
+    // create layer on map
+    createMap(L.layergroup(markers))
+}
+
+
+function createMap(markers) {
+    // tile layer for background
+    var maplayer = L.tileLayer("?");
+
+    // create baseMaps object to hold tile layer
+    var baseMaps = {
+
+    };
+    // create overlayMaps object for events layer
+    var overlayMaps ={
+        
+    };
+
+    // create map
+    var geoMap = L.map("map_area", {
+        center:
+        zoom:
+        preferCanvas:
+        layers: 
+    });
+
+    L.control.layer(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(geoMap)
+    
+
+}
