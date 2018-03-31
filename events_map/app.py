c26b5d1bf9b967ca14dd5c59150c7806d4f14120
akufoy <akufoy@gmail.com>
2018-03-30 23:42:37 -0500

    updating app.js

--- events_map/app.py
+++ events_map/app.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -14,9 +13,11 @@ from config import password
 app = Flask(__name__)
 # database setup
 Base = automap_base()
-engine = create_engine(f"postgresql+psycopg2://akufoy:{password}@rds-postgresql-bon-temps.capxvife87l2.us-east-2.rds.amazonaws.com:5432/bon_temps")
+
+engine = create_engine(f"postgresql+psycopg2://akufoy:{password}@rds-postgresql-bon-temps.capxvife87l2.us-east-2.rds.amazonaws.com/bon_temps")
 Base.prepare(engine, reflect=True)
 bon_temps = Base.classes.bon_temps
+
 session = Session(engine)
 
 # template rendering
@@ -27,8 +28,18 @@ def home():
 # route - leaflet
 @app.route("/api/leaflet_data")
 def leaflet_data():
-    data = session.query(bon_temps).all()
-    return data
+    results = session.query(bon_temps.venues, bon_temps.address, bon_temps.lat, bon_temps.lon).all()
+    # massage into json
+    bon_temps_data = []
+    for result in results:
+        venues = {}
+        venues["venue"] = result[0]
+        venues["address"] = result[1]
+        venues["lat"] = result[2]
+        venues["lon"] = result[3]
+        bon_temps_data.append(venues)
+
+    return jsonify(bon_temps_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- events_map/static/js/app.js
+++ events_map/static/js/app.js
@@ -1,47 +1,32 @@
-var container =  document.getElementById("map_area")
+// chart to see if database connection works
+var bon_link = "/api/leaflet_data"
+
+function dataTest(test) {
+    console.log(test[0].address);
+}
+d3.json(bon_link, dataTest);
 
 // leaflet javascript -
+// 
+// create map object
+var myMap = L.map("map", {
+    center: [30.26, -97.74],
+    zoom: 13
+});
 
-var queryEvents = "/api/leaflet_data"
-console.log(queryEvents)
+// add tile layer
+var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+}).addTo(myMap);
 
-// // create markers
-// d3.json(queryEvents, createMarkers);
-
-// function createMarkers(response) {
-    
-//     var markers = []
-//     // loop thru endpoint data to create markers
-
-//     // create layer on map
-//     createMap(L.layergroup(markers))
-// }
+// add markers
+var venueMarkers = L.marker([30.26, -97.74]).addTo(myMap);
 
 
-// function createMap(markers) {
-//     // tile layer for background
-//     var maplayer = L.tileLayer("?");
 
-//     // create baseMaps object to hold tile layer
-//     var baseMaps = {
 
-//     };
-//     // create overlayMaps object for events layer
-//     var overlayMaps ={
-        
-//     };
 
-//     // create map
-//     var geoMap = L.map("map_area", {
-//         center:
-//         zoom:
-//         preferCanvas:
-//         layers: 
-//     });
 
-//     L.control.layer(baseMaps, overlayMaps, {
-//         collapsed: false
-//     }).addTo(geoMap)
-    
 
-// }
+
