f13ba7067c3ac5312b3873319cef80abf4da1c07
akufoy <akufoy@gmail.com>
2018-04-02 16:51:21 -0500

    updating d3 elements

--- events_map/static/js/app.js
+++ events_map/static/js/app.js
@@ -5,12 +5,31 @@ d3.json(bon_link).then(function(data){
     for (i=0;i<data["results"].length;i++) {
         result = data["results"][i]
         var customPopup = `<strong>${result.venue}</strong><br>${result.address}`
+// <li class="list-group-item justify-content-between">${result.event}<br><span class="badge badge-default badge-pill">8:00 PM</span></li>
         var customOptions = {
             
         }
         var newMarker = new L.marker([result.lat, result.lon])
-        newMarker.bindPopup(customPopup, customOptions)                        
-        newMarker.addTo(myMap)
+        newMarker.bindPopup(customPopup, customOptions)
+        newMarker.venueName = `${result.venue}`
+        newMarker.venueAddress = `${result.address}`
+        newMarker.event = `${result.event}`
+        newMarker.addTo(myMap).on('click', onClick)
+        function onClick(e) {
+            // delete old entry
+            d3.selectAll(".auto-generate").remove()
+            // use d3. to populate venue-area with marker info on click
+            var venueList = d3.select(".list-group")
+                                .append("li")
+                                .attr("class", "auto-generate list-group-item justify-content-between")
+                                .text(e.target.event)
+                                .append("span")
+                                .attr("class", "badge badge-default badge-pill")
+                                .text("8:00 PM")
+
+            console.log(e.target.venueName)
+            // alert(this.getLatLng());
+        }
     }
 });
 
